77a18769884f1a93271cf2ab3f168379244c1a91
bowchen <bowchen@cisco.com>
2019-04-29 14:27:01 +0800

    Add codec G722 proj

--- vendor/g722/g722_fun.c
+++ vendor/g722/g722_fun.c
@@ -1,11 +1,10 @@
 #include "g722.h"
-#include "safe_mem_lib.h"
 
 void g722_init_decoder(g722Decoder *pDecoder)
 {
     if(pDecoder)
     {
-        cisco_memset_s(pDecoder, sizeof(g722Decoder), 0);
+        memset(pDecoder, 0, sizeof(g722Decoder));
         pDecoder->detl = 32;
         pDecoder->deth = 8;
         pDecoder->channels = 1;
@@ -16,7 +15,7 @@ void g722_init_encoder(g722Encoder *pEncoder, int frameSampleSize)
 {
     if(pEncoder)
     {
-        cisco_memset_s(pEncoder, sizeof(g722Encoder), 0);
+        memset(pEncoder, 0, sizeof(g722Encoder));
         pEncoder->detl = 32;
         pEncoder->deth = 8;
         pEncoder->frameCnt = 0;
